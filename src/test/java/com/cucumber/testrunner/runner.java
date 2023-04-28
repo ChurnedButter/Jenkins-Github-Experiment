1efbc922efc6061f4e7c43e890aa99e9b7d1123b
alexandereffanga <juxtaposedowl@gmail.com>
2023-04-28 13:57:13 -0500

    Slowly losing my mind

--- src/test/java/com/cucumber/testrunner/runner.java
+++ src/test/java/com/cucumber/testrunner/runner.java
@@ -4,9 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/resources/features"
-                ,glue = {"stepdefinition"}
-                ,tags = "@register"
-                ,plugin ={"pretty", "html:target/cucumber-reports/cucumber", "json:target/cucumber-reports/cucumber.json"})
+        ,glue = {"stepdefinition"}
+        ,tags = "@register"
+        ,plugin ={"pretty", "html:target/cucumber-reports/cucumber", "json:target/cucumber-reports/cucumber.json"}
+        ,monochrome = true)
 
 public class runner extends AbstractTestNGCucumberTests
 {}
